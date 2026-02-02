1b8e48965f2fcf6e6e49751d8bf1d8a266efab4d
Nikita Starikov <nikstarickov@gmail.com>
2026-02-02 14:57:05 +0300

    config: implement environment-specific configuration profiles

---
+++ src/main/java/com/example/demo_wallet_service/DemoWalletServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo_wallet_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoWalletServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoWalletServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demo_wallet_service/DemoWalletServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo_wallet_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoWalletServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
